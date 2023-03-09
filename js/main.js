c42232a360dd58f59ec1dfd3ef350963419c9c63
MyLadySnezhka <101964822+MyLadySnezhka@users.noreply.github.com>
2023-03-09 22:56:18 +0200

    Update main.js
    Edit check limits

--- js/main.js
+++ js/main.js
@@ -8,27 +8,32 @@ elPlayBoard.style.width = `1200px`;
 elPlayBoard.style.height = `800px`;
 
 const coord = () => {
-    X = parseInt(Math.random(10000)*1000);
-    Y = parseInt(Math.random(10000)*1000);
+    X = parseInt(Math.random()*1200) - 100;
+    Y = parseInt(Math.random()*800) - 100;
     //return (X, Y);
 }
 
-const maxX = parseInt(elPlayBoard.style.width) - 100;
-const maxY = parseInt(elPlayBoard.style.height) - 100;
+//const maxX = parseInt(elPlayBoard.style.width) - 100;
+//const maxY = parseInt(elPlayBoard.style.height) - 100;
 
 elPack.addEventListener('click', () => {
     elPack.classList.remove('packman');
     coord();
-    if((X>0)&&(X<maxX)&&(Y>0)&&(Y<maxY)) {
-        elWarn.innerHTML = '';
-            elPack.style.left = `${X}px`;
-            elPack.style.top = `${Y}px`;
-            elPack.classList.add('packman');
-        }
-    else {
-        elWarn.innerHTML = 'Вийшов за поле! Спробуй ще!'
-        elPack.style.left = `${maxX/2}px`;
-        elPack.style.top = `${maxY/2}px`;
-        elPack.classList.add('packman');
-    }
+    elPack.style.left = `${X}px`;          
+    elPack.style.top = `${Y}px`;
+    elPack.classList.add('packman');
+
+
+//    if((X>0)&&(X<maxX)&&(Y>0)&&(Y<maxY)) {
+//        elWarn.innerHTML = '';
+//            elPack.style.left = `${X}px`;
+//            elPack.style.top = `${Y}px`;
+//            elPack.classList.add('packman');
+//        }
+//    else {
+//        elWarn.innerHTML = 'Вийшов за поле! Спробуй ще!'
+//        elPack.style.left = `${maxX/2}px`;
+//        elPack.style.top = `${maxY/2}px`;
+//        elPack.classList.add('packman');
+//    }
 })
