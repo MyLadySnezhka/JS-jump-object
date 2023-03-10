cf68d019ced4581e5bdf07643a2388c6a3da6d92
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-03-10 10:52:56 +0200

    change limits

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const elPack = document.querySelector('.packman');
 const elPlayBoard = document.querySelector('.board');
 const elHouse = document.querySelector('.house');
 const elHelloText = document.querySelector('.hello');
+const elEndText = document.querySelector('.gameover');
 
 let X, Y;
 let houseX, houseY;
@@ -13,8 +14,8 @@ let houseX, houseY;
 elPlayBoard.style.width = `800px`;
 elPlayBoard.style.height = `600px`;
 //розмір ігрового елемента
-elPack.style.width = `200px`;
-elPack.style.height = `200px`;
+elPack.style.width = `150px`;
+elPack.style.height = `150px`;
 //розмір конури
 elHouse.style.width = `200px`;
 elHouse.style.height = `200px`;
@@ -22,14 +23,17 @@ elHouse.style.height = `200px`;
 const maxX = parseInt(elPlayBoard.style.width) - parseInt(elPack.style.width);
 const maxY = parseInt(elPlayBoard.style.height) - parseInt(elPack.style.height);
 
+const maxHouseX = parseInt(elPlayBoard.style.width) - parseInt(elHouse.style.width);
+const maxHouseY = parseInt(elPlayBoard.style.height) - parseInt(elHouse.style.height);
+
 const coord = () => {
     X = parseInt(Math.random()*maxX);
     Y = parseInt(Math.random()*maxY);
 }
 
 const coordHouse = (lnkHouse) => {
-    houseX = parseInt(Math.random()*maxX);
-    houseY = parseInt(Math.random()*maxY);
+    houseX = parseInt(Math.random()*maxHouseX);
+    houseY = parseInt(Math.random()*maxHouseY);
     lnkHouse.style.left = `${houseX}px`;
     lnkHouse.style.top = `${houseY}px`;
 }
@@ -37,15 +41,16 @@ const coordHouse = (lnkHouse) => {
 coordHouse(elHouse);
 console.log('house', houseX, houseY);
 
-elPack.addEventListener('click', () => {
+elPack.addEventListener('click', (ev) => {
     elPack.classList.remove('packman');
     coord();
     console.log('dog', X, Y);
     elPack.style.left = `${X}px`;          
     elPack.style.top = `${Y}px`;
     elPack.classList.add('packman');
-        if(((X<=houseX-50)||(X>=houseX+50))&&((Y<=houseY-50)||(Y>=houseY+50))) {
-            elHelloText.innerHTML = 'Вітаю, цуценя вдома!';
+        if((X>=houseX-50)&&(X<=houseX+100)&&(Y>=houseY-50)&&(Y<=houseY+120)) {
+            elEndText.innerHTML = `<h2>Вітаю, цуценя вдома!</h2><h3>Якщо хочеш зіграти ще, натисни F5.</h3>`;
+            elPack.removeEventListener('click', (ev));
             return;
         }
 })
