e414fb23a65ebd08b61a51908eb768c4df00bbab
MyLadySnezhka <mylady.snezhka@gmail.com>
2023-03-10 09:55:38 +0200

    add house

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 const elPack = document.querySelector('.packman');
 const elPlayBoard = document.querySelector('.board');
+const elHouse = document.querySelector('.house');
+const elHelloText = document.querySelector('.hello');
 
 let X, Y;
+let houseX, houseY;
 
 // похідні дані, можна запропонувати їх дати обирати користувачеві 
 //(тоді ще додати перевірку, щоб розмір елемента не міг бути більше, ніж розмір дошки)
 
 //розмір дошки
-elPlayBoard.style.width = `1200px`;
-elPlayBoard.style.height = `800px`;
+elPlayBoard.style.width = `800px`;
+elPlayBoard.style.height = `600px`;
 //розмір ігрового елемента
-elPack.style.width = `100px`;
-elPack.style.height = `100px`;
+elPack.style.width = `200px`;
+elPack.style.height = `200px`;
+//розмір конури
+elHouse.style.width = `200px`;
+elHouse.style.height = `200px`;
 
 const maxX = parseInt(elPlayBoard.style.width) - parseInt(elPack.style.width);
 const maxY = parseInt(elPlayBoard.style.height) - parseInt(elPack.style.height);
@@ -21,12 +27,27 @@ const coord = () => {
     Y = parseInt(Math.random()*maxY);
 }
 
+const coordHouse = (lnkHouse) => {
+    houseX = parseInt(Math.random()*maxX);
+    houseY = parseInt(Math.random()*maxY);
+    lnkHouse.style.left = `${houseX}px`;
+    lnkHouse.style.top = `${houseY}px`;
+}
+
+coordHouse(elHouse);
+console.log('house', houseX, houseY);
+
 elPack.addEventListener('click', () => {
     elPack.classList.remove('packman');
     coord();
+    console.log('dog', X, Y);
     elPack.style.left = `${X}px`;          
     elPack.style.top = `${Y}px`;
     elPack.classList.add('packman');
+        if(((X<=houseX-50)||(X>=houseX+50))&&((Y<=houseY-50)||(Y>=houseY+50))) {
+            elHelloText.innerHTML = 'Вітаю, цуценя вдома!';
+            return;
+        }
 })
 
 
